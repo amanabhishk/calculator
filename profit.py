6a340200a496c6f648f3903e4994449cb0dc25c2
Aman Abhishek <aabhishek@wisc.edu>
2017-12-09 15:42:33 -0600

    more info printed.

--- profit.py
+++ profit.py
@@ -1,3 +1,4 @@
+import numpy as np
 import sys
 BTC, LTC, ETH  = dict(), dict(), dict()
 
@@ -10,22 +11,30 @@ ETH["Dec07"] = [100,97.01,431.65]
 
 def gain(d,PRICE):
     PRICE = float(PRICE)
-    spent, gained = 0, 0
+    spent, gained = list(), list()
     for index, key in enumerate(d):
-        spent += d[key][0]
-        gained += (d[key][1]/d[key][2])*PRICE
-    return spent, gained, gained-spent
-
+        x = d[key][0]
+        spent.append(x)
+        y = (d[key][1]/d[key][2])*PRICE
+        gained.append(y)
+        print("{0} {1:.0f} {2:.0f} {3:.0f}".format(key,x,y,y-x))
+    print("Total {0:.0f} {1:.0f} {2:.0f}".format(sum(spent),sum(gained),sum(gained)-sum(spent)))
+    return np.array(spent), np.array(gained), np.array(gained)-np.array(spent)
 
 if __name__=="__main__":
     if len(sys.argv) != 4:
         print("$python profit.py BTC_price ETH_price LTC_price")
     else:
-        print("BTC:")
-        print(gain(BTC,sys.argv[1]))
-        print("ETH")
-        print(gain(ETH,sys.argv[2]))
-        print("LTC:")
-        print(gain(LTC,sys.argv[3]))
+        print("\n\nBTC:")
+        BTC_= gain(BTC,sys.argv[1])
+        print("\nETH:")
+        ETH_ = gain(ETH,sys.argv[2])
+        print("\nLTC:")
+        LTC_ = gain(LTC,sys.argv[3])
 
+        print("--------")
+        x = BTC_[0].sum()+LTC_[0].sum()+ETH_[0].sum()
+        y = BTC_[1].sum()+LTC_[1].sum()+ETH_[1].sum()
+        print("Total {0:.0f} {1:.0f} {2:.0f}".format(x,y,y-x))
+        print("\n")
 
